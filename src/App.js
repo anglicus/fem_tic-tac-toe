96dbd47b5324ddf3ec36c9bbbd7f6e3852afc6d6
anglicus <anglicus@yahoo.com>
2022-07-27 16:31:03 +0900

    work on layout, scoring method

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 
 import "./css/app.css";
 
+///////////////////////////////////////////////////////////////////////////////////////////
+
 const Button = (props) => {
   return (
     <button
@@ -13,6 +15,8 @@ const Button = (props) => {
   );
 };
 
+///////////////////////////////////////////////////////////////////////////////////////////
+
 const ScoreBox = (props) => {
   return (
     <div className={"score-box " + props.color}>
@@ -22,6 +26,8 @@ const ScoreBox = (props) => {
   );
 };
 
+///////////////////////////////////////////////////////////////////////////////////////////
+
 const PlaySquare = (props) => {
   return (
     <div className="play-square" id={"square-" + props.id.toString()}>
@@ -29,6 +35,33 @@ const PlaySquare = (props) => {
     </div>
   );
 };
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+const BannerModal = ({ show, parameters }) => {
+  const showHideClassName = show ? "open" : "closed";
+  return (
+    <div className={"banner-modal " + showHideClassName}>
+      <h2>{parameters.heading}</h2>
+      <Button
+        clickfunction={parameters.button1Function}
+        parameters={null}
+        color="btn-silver"
+        size="btn-secondary"
+        label={parameters.button1Text}
+      />
+      <Button
+        clickfunction={parameters.button2Function}
+        parameters={null}
+        color="btn-yellow"
+        size="btn-secondary"
+        label={parameters.button2Text}
+      />
+    </div>
+  );
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////
 
 const NewGameDisplay = (props) => {
   const [mark, setMark] = useState("x");
@@ -74,44 +107,80 @@ const NewGameDisplay = (props) => {
   );
 };
 
+///////////////////////////////////////////////////////////////////////////////////////////
+
 const GameDisplay = (props) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [roundInProgress, setRoundInProgress] = useState(true);
+
+  const handleRoundEnd = () => {};
+
+  const showModal = () => {
+    setModalOpen(true);
+  };
+
+  const hideModal = () => {
+    setModalOpen(false);
+  };
+
+  const resetModalParameters = {
+    heading: "restart game?",
+    button1Text: "no, cancel",
+    button1Function: hideModal,
+    button2Text: "yes, restart",
+    button2Function: props.resetFunction,
+  };
+
   const playSquares = [];
   for (let i = 1; i < 10; i++) {
     playSquares.push(<PlaySquare key={i} id={i} />);
   }
 
   return (
-    <div className="game-display">
-      <div className="game-display__icons">xo</div>
-      <div className="game-display__turn-display">turn</div>
-      <Button
-        // clickfunction={props.startGame}
-        // parameters={{ p1: mark, p2: "cpu" }}
-        color="btn-silver"
-        size="btn-reset"
-        label=""
-      />
-      {playSquares}
-      <ScoreBox
-        player={"x (" + props.players.x + ")"}
-        score={props.score.x}
-        color="blue"
-      />
-      <ScoreBox player="ties" score={props.score.ties} color="silver" />
-      <ScoreBox
-        player={"o (" + props.players.o + ")"}
-        score={props.score.x}
-        color="yellow"
-      />
+    <div>
+      <BannerModal show={modalOpen} parameters={resetModalParameters} />
+      <div className="game-display">
+        <div className="game-display__icons">xo</div>
+        <div className="game-display__turn-display">turn</div>
+        <Button
+          clickfunction={showModal} // {props.resetFunction}
+          parameters={null}
+          color="btn-silver"
+          size="btn-reset"
+          label=""
+        />
+        {playSquares}
+        <ScoreBox
+          player={"x (" + props.players.x + ")"}
+          score={props.score.x}
+          color="blue"
+        />
+        <ScoreBox player="ties" score={props.score.ties} color="silver" />
+        <ScoreBox
+          player={"o (" + props.players.o + ")"}
+          score={props.score.x}
+          color="yellow"
+        />
+      </div>
+      <button onClick={() => props.scoreFunction("x")}>score for x</button>
+      <button onClick={() => props.scoreFunction("o")}>score for y</button>
+      <button onClick={() => props.scoreFunction("ties")}>
+        score for ties
+      </button>
     </div>
   );
 };
 
+///////////////////////////////////////////////////////////////////////////////////////////
+
 function App() {
   const [gameInProgress, setGameInProgress] = useState(false);
-  const [roundInProgress, setRoundInProgress] = useState(false);
   const [score, setScore] = useState({ x: 0, o: 0, ties: 0 });
   const [players, setPlayers] = useState({ x: null, o: null });
+
+  const handleReset = () => {
+    setGameInProgress(false);
+  };
 
   const handleStartGame = (playerOptions) => {
     // playerOptions: object with p1 and p2 properties
@@ -130,11 +199,34 @@ function App() {
     setPlayers({ x: newPlayerSettings.x, o: newPlayerSettings.o });
   };
 
+  const handleIncrementScore = (scoringMark) => {
+    console.log("old score was:", score);
+    const newScore = { x: 0, o: 0, ties: 0 };
+    Object.keys(newScore).forEach((mark) => {
+      if (mark === scoringMark) {
+        newScore[mark] = score[mark] + 1;
+      } else {
+        newScore[mark] = score[mark];
+      }
+    });
+    console.log("new score is:", newScore);
+    setScore(newScore);
+  };
+
   if (gameInProgress) {
-    return <GameDisplay players={players} score={score} />;
+    return (
+      <GameDisplay
+        players={players}
+        score={score}
+        resetFunction={handleReset}
+        scoreFunction={handleIncrementScore}
+      />
+    );
   } else {
     return <NewGameDisplay startGame={handleStartGame} />;
   }
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////
 
 export default App;
